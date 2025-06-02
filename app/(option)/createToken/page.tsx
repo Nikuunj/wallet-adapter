2391390007604f3b784aa29ca9fc126dac970a80
Nikunj <mnikunj262@gmail.com>
2025-06-03 00:16:49 +0530

    [Update]: Create token function to create a own token, but meta data not attach

--- app/(option)/createToken/page.tsx
+++ app/(option)/createToken/page.tsx
@@ -3,18 +3,99 @@
 import Button from "@/components/Button";
 import InputBox from "@/components/InputBox";
 import Loading from "@/components/Loading";
+import { createInitializeMetadataPointerInstruction, createInitializeMintInstruction, ExtensionType, getMintLen, LENGTH_SIZE, TOKEN_2022_PROGRAM_ID, TYPE_SIZE } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { useRef, useState } from "react";
-
+import toast from "react-hot-toast";
+import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 
 function CreateToken() {
 
     const refInputArr = useRef<HTMLInputElement[] | null[]>(Array(4).fill(0));
     const [loading, setLoading] = useState<boolean>(false);
+    const wallet = useWallet();
+    const { connection } = useConnection();
 
     async function createToken() {
-        console.log('click me');
-        setLoading(false);
-        
+        if (!wallet.connected || !wallet.publicKey) {
+            console.error("Wallet not connected");
+            return;
+        }
+        // const tokenName = refInputArr.current[0]?.value;
+        // const tokenSymbol = refInputArr.current[1]?.value;
+        // const tokenImageUrl = refInputArr.current[2]?.value;
+        // const initialSupply = refInputArr.current[3]?.value;
+
+        // if (!tokenName || !tokenSymbol || !tokenImageUrl || !initialSupply) {
+        //     console.error("Please fill in all fields");
+        //     toast.error("Please fill in all fields.");
+        //     return;
+        // }
+
+        try {
+            setLoading(true);
+            
+            const mintkeyPair = Keypair.generate();
+             const metadata = {
+                mint: mintkeyPair.publicKey,
+                name: 'OPOSSS',
+                symbol: 'OPOSSS',
+                uri: 'https://cdn.100xdevs.com/metadata.json',
+                additionalMetadata: [],
+            };
+
+            const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+            const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+            const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+            const transaction = new Transaction();
+            transaction.add(
+                SystemProgram.createAccount ({
+                    fromPubkey: wallet.publicKey,
+                    newAccountPubkey: mintkeyPair.publicKey,
+                    space: mintLen,
+                    lamports,
+                    programId: TOKEN_2022_PROGRAM_ID,
+                }),
+                createInitializeMetadataPointerInstruction(
+                    mintkeyPair.publicKey, 
+                    wallet.publicKey, 
+                    mintkeyPair.publicKey, 
+                    TOKEN_2022_PROGRAM_ID
+                ),
+                createInitializeMintInstruction(
+                    mintkeyPair.publicKey, 
+                    9, 
+                    wallet.publicKey, 
+                    null, 
+                    TOKEN_2022_PROGRAM_ID
+                ),
+                createInitializeInstruction({
+                    programId: TOKEN_2022_PROGRAM_ID,
+                    mint: mintkeyPair.publicKey,
+                    metadata: mintkeyPair.publicKey,
+                    name: metadata.name,
+                    symbol: metadata.symbol,
+                    uri: metadata.uri,
+                    mintAuthority: wallet.publicKey,
+                    updateAuthority: wallet.publicKey,
+                }),
+            );
+            // https://ik.imagekit.io/jh5fuaux3/images/E__pr_one1app_server_public_temp_1748872676155-logo_yO9oNZRZ2j.png
+            transaction.feePayer = wallet.publicKey;
+            const recentBlock = await connection.getLatestBlockhash();
+            transaction.recentBlockhash = recentBlock.blockhash;
+            transaction.partialSign(mintkeyPair);
+            const sign = await wallet.sendTransaction(transaction, connection);
+            console.log(sign);
+            console.log(mintkeyPair.publicKey.toBase58());
+        } catch (error) {
+            console.error("Error creating token:", error);
+            toast.error("Failed to create token. Please check the console for details.");
+            return;
+        } finally {
+            setLoading(false);
+        }    
     }
 
     return (
@@ -22,11 +103,13 @@ function CreateToken() {
             <div>
                 <h1>Create Token</h1>
                 
-                <Button handleClick={createToken} text={'create token'}/>
-                <InputBox reference={(e) => refInputArr.current[0] = e} text={`Massage..`} typeOfInp={'text'}/>
-                <InputBox reference={(e) => refInputArr.current[1] = e} text={`Massage..`} typeOfInp={'text'}/>
-                <InputBox reference={(e) => refInputArr.current[2] = e} text={`Massage..`} typeOfInp={'text'}/>
-                <InputBox reference={(e) => refInputArr.current[3] = e} text={`Massage..`} typeOfInp={'text'}/>
+                <div className="w-72 gap-2 flex flex-col">
+                    <InputBox reference={(e) => refInputArr.current[0] = e} text={`Token Name`} typeOfInp={'text'}/>
+                    <InputBox reference={(e) => refInputArr.current[1] = e} text={`Token Symbol`} typeOfInp={'text'}/>
+                    <InputBox reference={(e) => refInputArr.current[2] = e} text={`Token Image URL`} typeOfInp={'text'}/>
+                    <InputBox reference={(e) => refInputArr.current[3] = e} text={`Initial Supply - Eg. 0`} typeOfInp={'text'}/>
+                    <Button handleClick={createToken} text={'create token'}/>
+                </div>
             </div>
 
             {loading && (
