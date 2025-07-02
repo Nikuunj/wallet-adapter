9672285a3272988cd0cc79f7d1198b4d26a55f7f
Nikunj <mnikunj262@gmail.com>
2025-07-02 23:52:14 +0530

    [update]: swap token data

--- app/(option)/swap/page.tsx
+++ app/(option)/swap/page.tsx
@@ -1,7 +1,50 @@
+"use client"
+
+import { useSolanaNetwork } from "@/components/WalletContexProvide";
+import { tokenData } from "@/utils/tokenData";
+import axios from "axios"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast";
+
+export interface TokenTypes {
+  chainId: number;
+  address: string;
+  symbol: string;
+  name: string;
+  decimals: number;
+  logoURI: string;
+  tags: string[];
+  extensions?: Record<string, string>;
+}
 
 function SwapToken() {
+     const { network } = useSolanaNetwork();
+     const [inputToken, setInputToken] = useState<string>("So11111111111111111111111111111111111111111")
+     const [outputToken, setOutputToken] = useState<string>("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+
+
      return (
-          <div>Coming soon...</div>
+          <div className="flex flex-col">
+               <select
+               onChange={(e) => { 
+                    setInputToken(e.target.value)
+               }}
+               value={inputToken}>
+                    {tokenData.map((token, idx) => (
+                         <option key={token.address + idx} value={token.address}>{token.symbol}</option>
+                    ))}
+               </select>
+
+               <select
+               onChange={(e) => { 
+                    setOutputToken(e.target.value)
+               }}
+               value={outputToken}>
+                    {tokenData.map((token, idx) => (
+                         <option key={token.address + idx} value={token.address}>{token.symbol}</option>
+                    ))}
+               </select>
+          </div>
      )
 }
 
